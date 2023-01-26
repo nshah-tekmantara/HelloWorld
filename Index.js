ab6944218c97252722621e59dc9919c2a9bab619
Nirav Shah <NS@NS.COM>
2023-01-26 15:16:35 -0500

    Simulate simple calculator

--- Index.js
+++ Index.js
@@ -1,6 +1,13 @@
-console.log('Hello World');
+console.log('Simple Calculator');
+var x = parseInt(Math.random(0, 1) * 100);
+var y = parseInt(Math.random(0, 1) * x);
 
-console.log(addNumbers(100, 200));
+console.log('Simple Calculator for 2 random numbers ', x, y);
+
+console.log('Add ', addNumbers(x, y));
+console.log('Subtract ', subtractNumbers(x, y));
+console.log('Multiply ', multiplyNumbers(x, y));
+console.log('Divide ', divideNumbers(x, y));
 
 function addNumbers(x, y) {
   return x + y;
@@ -15,9 +22,14 @@ function subtractNumbers(x, y) {
 }
 
 function divideNumbers(x, y) {
-  if (y < 1) {
-    return x / y;
+  if (y > 0) {
+    return parseFloat((x / y).toFixed(2));
   } else {
-    console.warn("division can't be performed with zero or negative numbers");
+    console.warn(
+      "division can't be performed with zero or negative numbers",
+      x,
+      y,
+      y < 1
+    );
   }
 }
