cf76e0d62e4c1bd0dce1ed8b9255a631b948cdae
Nirav Shah <NS@NS.COM>
2023-01-26 14:56:18 -0500

    Feature-SubtractNumbers

--- Index.js
+++ Index.js
@@ -6,6 +6,10 @@ function addNumbers(x, y) {
   return x + y;
 }
 
-function multiplyNumbers(x,y){
-    return x*y;
+function multiplyNumbers(x, y) {
+  return x * y;
+}
+
+function subtractNumbers(x, y) {
+  return x - y;
 }
